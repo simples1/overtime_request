e8fb199a780dbf4fcf6a7391d85b9a38cc6ab826
abdullah <abdullaaden@outlook.com>
2016-08-10 05:54:00 +0100

    adds asociations between User and Posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -16,6 +16,8 @@ describe 'navigate' do
 
    describe 'creation' do
    	before do
+			user = User.create(email: "aa@aa.com", password: "12345678", password_confirmation: "12345678", first_name: "jim", last_name: "hum")
+			login_as(user, :scope => :user)
   		visit new_post_path
    	end
 
@@ -26,10 +28,17 @@ describe 'navigate' do
   	it "can be created from new form page" do 
   		fill_in "post[date]", with: Date.today
   		fill_in "post[rationale]", with: "Rationale stuff!"
-
   		click_on "Save"
 
   		expect(page).to have_content("Rationale stuff!")
+  	end
+
+  	it "will have a use associated with it" do 
+  		fill_in "post[date]", with: Date.today
+  		fill_in "post[rationale]", with: "Rationale stuff12!"
+  		click_on "Save"
+
+  		expect(User.last.posts.last.rationale).to eq("Rationale stuff12!")
   	end
   end
 end
