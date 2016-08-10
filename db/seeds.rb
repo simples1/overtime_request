bbdda17b0d2e6490b55273cc4aad70929ae20f0a
abdullah <abdullaaden@outlook.com>
2016-08-10 04:54:44 +0100

    adds seed for 100 Post entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |post|
+	Post.create(date: Date.today, rationale: "#{post} Anything Content")
+end
---
+++ spec/features/post_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'navigate' do
+  describe 'index' do
+  	it "can be reached" do 
+  		visit posts_path
+  		expect(page.status_code).to eq(200)
+  	end
+
+  	it "has title of Posts" do 
+  		visit posts_path
+  		expect(page).to have_content(/Posts/)
+  	end
+
+
+  end
+end
