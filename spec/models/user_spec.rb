34314356b86ec89e4a01f202376a5c09d96160cc
abdullah <abdullaaden@outlook.com>
2016-08-10 04:33:59 +0100

    adds user validations firstname and lastname

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe "creation" do
+		before do
+			@user = User.create(email: "aa@aa.com", password: "12345678", password_confirmation: "12345678", first_name: "jim", last_name: "hum")
+		end
+
+		it "can be created" do 
+			expect(@user).to be_valid
+		end
+
+		it "cannot be created without first_name or last_name" do
+			@user.first_name = nil
+			@user.last_name = nil
+			expect(@user).to_not be_valid
+		end
+	end
 end
