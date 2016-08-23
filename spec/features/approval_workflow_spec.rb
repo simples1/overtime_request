ae05397e5c7876a09aab0cf621c963ce04172fe3
abdullah <abdullaaden@outlook.com>
2016-08-24 00:25:47 +0100

    hides status radio buttons from Non-Admin Users

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -10,15 +10,27 @@ describe "navigate" do
 	describe "edit" do 
 		before do
 			@post = FactoryGirl.create(:post)
+			visit edit_post_path(@post)
 		end
 
-		it "has a status that can be edited onthe form" do 
-			visit edit_post_path(@post)
+		it "has a status that can be edited onthe form by an admin" do 
 
 			choose 'post_status_approved'
 			click_on("Save")
 			
 			expect(@post.reload.status).to eq("approved")
+		end
+
+
+		it "can not be edit by a non-Admin" do
+			logout(:user)
+			user = FactoryGirl.create(:user)
+			login_as(user, scope: :user)
+
+			visit edit_post_path(@post)
+
+			expect(page).to_not have_content("Approved")
+
 		end
 	end
 end
